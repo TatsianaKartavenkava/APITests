856b27f39ccdd72f3e868f8f121c1f8634039520
Tatya <tatyana.orehva@mail.ru>
2021-06-22 17:05:13 +0300

    Task1

---
+++ src/test/java/RestTest.java
@@ -0,0 +1,25 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.testng.annotations.Test;
+
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class RestTest {
+    @Test
+    public void getRequest() {
+        given().log().everything()
+                .baseUri("https://swapi.dev/api/planets/")
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then().log().everything()
+                .assertThat()
+                .statusCode(200)
+                .body("count", is(61));
+
+
+    }
+
+}
