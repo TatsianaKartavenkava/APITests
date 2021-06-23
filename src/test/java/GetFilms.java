ecd8e1fbaaf0109cb119f8d61aa1d283689dfc18
Tatya <tatyana.orehva@mail.ru>
2021-06-23 15:56:32 +0300

    Task3

---
+++ src/test/java/GetFilms.java
@@ -0,0 +1,23 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class GetFilms {
+
+    @Test
+    public static void getFilms() {
+        List<Object> title = given().log().everything()
+                .baseUri("https://swapi.dev/api/films/")
+                .contentType(ContentType.JSON)
+                .when().get()
+                .then().log().everything()
+                .assertThat()
+                .statusCode(200)
+                .extract().jsonPath().getList("results.title");
+    }
+
+}
