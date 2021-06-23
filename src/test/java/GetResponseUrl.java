9bacc1f83d3b2e3eaf319d6a520f1cee6995332b
Tatya <tatyana.orehva@mail.ru>
2021-06-23 08:49:31 +0300

    Task2 modified

--- src/test/java/GetResponseUrl.java
+++ src/test/java/GetResponseUrl.java
@@ -3,19 +3,26 @@ import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Condition;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-public class getResponseUrl {
+public class GetResponseUrl {
     @Test
-    public static void getResponseHeaders() {
+    public static void GetResponseHeaders() {
         given().log().everything()
                 .baseUri("https://swapi.dev/api/planets/")
                 .contentType(ContentType.JSON)
                 .when().get("/1/")
+                .then().log().everything();
+
+        given().log().everything()
+                .when().get("https://swapi.dev/api/planets/1/")
                 .then().log().everything()
                 .assertThat()
                 .statusCode(200);
+
+
 
     }
 }
