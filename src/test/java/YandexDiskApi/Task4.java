5b186a45243a431cff308bea5f8990a6843ed9d5
Tatya <tatyana.orehva@mail.ru>
2021-06-29 16:46:41 +0300

    1. Создать папку на яндекс диске 2. Внутри созданной папки создать файл 3. Поместить созданный файл в корзину 4. Восстановить созданный файл из корзины 5. Удалить файл и папку

---
+++ src/test/java/YandexDiskApi/Task4.java
@@ -0,0 +1,20 @@
+package YandexDiskApi;
+/*1. Создать папку на яндекс диске
+2. Внутри созданной папки создать файл
+3. Поместить созданный файл в корзину
+4. Восстановить созданный файл из корзины
+5. Удалить файл и папку*/
+import org.testng.annotations.Test;
+
+public class Task4 {
+    @Test
+    public static void task3() {
+        RestApiManager restApiManager = new RestApiManager();
+        restApiManager.createFolder();
+        restApiManager.makeFile1();
+        restApiManager.deleteFile1();
+
+
+
+    }
+}
