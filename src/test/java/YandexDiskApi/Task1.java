d592fb1a51f23789f1c9b01e219b563633d2ba80
Tatya <tatyana.orehva@mail.ru>
2021-06-30 13:09:27 +0300

    1. Создать папку на диске 2. Удалить созданную папку 3. Проверить, что папку удалена

--- src/test/java/YandexDiskApi/Task1.java
+++ src/test/java/YandexDiskApi/Task1.java
@@ -1,10 +1,15 @@
+/*1. Создать папку на диске
+2. Удалить созданную папку
+3. Проверить, что папку удалена
+Чтобы включить программу чтения с экрана, нажмите Ctrl+Alt+Z. Для просмотра списка быстрых клавиш нажмите Ctrl+косая черта.
+*/
 package YandexDiskApi;
 
 import org.testng.annotations.Test;
 
 public class Task1 {
     @Test
-    public static void folder() {
+    public static void task1() {
         RestApiManager restApiManager = new RestApiManager();
         restApiManager.createFolder();
         restApiManager.deleteFolder();
